cef472016d09c06f7a3375be8c80eb4789b21954
Lukasz Romerowicz <lukasz.romerowicz@stockport.gov.uk>
2019-07-12 12:55:28 +0100

    fix: Amend request property name

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ exports.handler = async (event) => {
         const redisUrl = process.env.REDIS_URL
         const client = await connectToRedis(redisUrl)
         const delAsync = promisify(client.del).bind(client)
-        await delAsync(parsedRequest.key)
+        await delAsync(parsedRequest.Key)
 
         return {
             statusCode: 200
